86d4948e559c24f8ef51bdf48f412ec0773a5979
kalai135 <kalaiarumugham135@gmail.com>
2024-03-31 12:35:45 +0530

    Creation of Variables

---
+++ CoreJava/src/com/nexa/javabasics/VariableDemo.java
@@ -0,0 +1,18 @@
+package com.nexa.javabasics;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println(5+4);
+		
+		
+		int num1 = 3;
+		int num2 = 4;
+		
+		int result = num1 + num2;
+		System.out.println(result);
+
+	}
+
+}
