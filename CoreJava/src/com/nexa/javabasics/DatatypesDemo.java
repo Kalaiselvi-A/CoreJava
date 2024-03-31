b05819c9863dad12d3b17551cfe74691708ce47b
kalai135 <kalaiarumugham135@gmail.com>
2024-03-31 12:44:29 +0530

    DataTypes in Java

---
+++ CoreJava/src/com/nexa/javabasics/DatatypesDemo.java
@@ -0,0 +1,26 @@
+package com.nexa.javabasics;
+
+public class DatatypesDemo {
+
+	public static void main(String[] args) {
+		
+		//Integer
+		int num = 34;
+		byte by = 127;
+		short sh = 558;
+		long l = 45678;
+		
+		//Float
+		float f = 4.6f;
+		double d = 5.8;
+		
+		//Character
+		char c = 'k';
+		char ch = '5';	//literals
+		
+		//Boolean
+		boolean b = true;
+		
+	}
+
+}
